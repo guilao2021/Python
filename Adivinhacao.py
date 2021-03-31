a6055fc328490725d4ac55a0f9e922ccc602ce74
Guilherme <guiad2013@gmail.com>
2021-03-31 14:04:24 -0300

    Acrescentando condiçoes

--- Adivinhacao.py
+++ Adivinhacao.py
@@ -6,10 +6,19 @@ numero_secreto = 40
 
 chute = int(input("Digite um número: "))
 
-if (numero_secreto == chute):
+chute_acertou = chute == numero_secreto
+
+chute_maior   = chute > numero_secreto
+
+chute_menor   = chute < numero_secreto
+
+if (chute_acertou):
     print("Você acertou! O número secreto era",numero_secreto,".")
 
 else:
-    print("Você errou!! O número secreto era",numero_secreto,".")
+    if(chute_maior):
+        print("Você errou!! O número secreto era menor.")
+    elif(chute_menor):
+        print("Você errou!! O número secreto era maior.")
 
 print("FIM DO JOGO")
