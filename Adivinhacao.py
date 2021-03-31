7537b322c778fb01cdaa884eb49adb9742bd26a1
Guilherme <guiad2013@gmail.com>
2021-03-31 14:40:52 -0300

    Acrescentando mais tentativas para o usuário.

--- Adivinhacao.py
+++ Adivinhacao.py
@@ -4,21 +4,24 @@ print("---------------------------------")
 
 numero_secreto = 40
 
-chute = int(input("Digite um número: "))
+#Contador
+rodada = 1
+total_tentativas = 3
 
-chute_acertou = chute == numero_secreto
-
-chute_maior   = chute > numero_secreto
-
-chute_menor   = chute < numero_secreto
-
-if (chute_acertou):
-    print("Você acertou! O número secreto era",numero_secreto,".")
-
-else:
-    if(chute_maior):
-        print("Você errou!! O número secreto era menor.")
-    elif(chute_menor):
-        print("Você errou!! O número secreto era maior.")
+while (rodada <= total_tentativas):
+    print("** Tentativa",rodada,"de",total_tentativas,"**")
+    # Variáveis
+    chute = int(input("Digite um número: "))
+    chute_acertou = chute == numero_secreto
+    chute_maior = chute > numero_secreto
+    chute_menor = chute < numero_secreto
+    if (chute_acertou):
+        print("Você acertou! O número secreto era",numero_secreto,".")
+    else:
+        if(chute_maior):
+            print("Você errou!! O número secreto era menor.")
+        elif(chute_menor):
+            print("Você errou!! O número secreto era maior.")
+    rodada += 1
 
 print("FIM DO JOGO")
