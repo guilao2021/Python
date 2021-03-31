949f3eb249893382985f061c1ee8e519bccb263b
Guilherme <guiad2013@gmail.com>
2021-03-31 16:22:06 -0300

    Melhorando codificação

--- Adivinhacao.py
+++ Adivinhacao.py
@@ -8,20 +8,15 @@ numero_secreto = 40
 rodada = 1
 total_tentativas = 3
 
-while (rodada <= total_tentativas):
-    print("** Tentativa",rodada,"de",total_tentativas,"**")
-    # Variáveis
+for rodada in range (rodada,total_tentativas + 1):
+    print("** Tentativa {} de {} **".format(rodada,total_tentativas))
     chute = int(input("Digite um número: "))
-    chute_acertou = chute == numero_secreto
-    chute_maior = chute > numero_secreto
-    chute_menor = chute < numero_secreto
-    if (chute_acertou):
-        print("Você acertou! O número secreto era",numero_secreto,".")
+    if (chute == numero_secreto):
+        print("Você acertou! O número secreto era {}.".format(numero_secreto))
     else:
-        if(chute_maior):
+        if(chute > numero_secreto):
             print("Você errou!! O número secreto era menor.")
-        elif(chute_menor):
+        elif(chute < numero_secreto):
             print("Você errou!! O número secreto era maior.")
-    rodada += 1
 
 print("FIM DO JOGO")
