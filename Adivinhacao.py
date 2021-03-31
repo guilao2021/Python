f94edfffa213c7823ef075bed6de3dd3f129a99b
Guilherme <guiad2013@gmail.com>
2021-03-31 16:46:48 -0300

    Alterando formatação do código

--- Adivinhacao.py
+++ Adivinhacao.py
@@ -8,11 +8,15 @@ numero_secreto = 40
 rodada = 1
 total_tentativas = 3
 
-for rodada in range (rodada,total_tentativas + 1):
+for rodada in range (1,total_tentativas + 1):
     print("** Tentativa {} de {} **".format(rodada,total_tentativas))
-    chute = int(input("Digite um número: "))
+    chute = int(input("Digite um número de 1 á 50: "))
+    if (chute < 1 or chute > 50):
+        print("Você deve digitar um número entre 1 e 50: ")
+        continue;
     if (chute == numero_secreto):
         print("Você acertou! O número secreto era {}.".format(numero_secreto))
+        break;
     else:
         if(chute > numero_secreto):
             print("Você errou!! O número secreto era menor.")
