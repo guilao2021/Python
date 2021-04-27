d84c0c492320867e8d17332fcb4b420d53e5f406
Guilherme <guiad2013@gmail.com>
2021-04-27 14:13:09 -0300

    Refatorando o código

--- CPF_CNPJ.py
+++ CPF_CNPJ.py
@@ -1,55 +1,62 @@
 from validate_docbr import CPF, CNPJ
 
-class ValidaCpf:
-    def __init__(self, cpf):
-        self.analisa_cpf(cpf)
+class Documento:
 
-    def analisa_cpf(self, cpf):
-        if len(cpf) == 11:
-            analisa_cpf = CPF()
-            if analisa_cpf.validate(cpf):
-                self.cpf = cpf
-                print('CPF salvo')
-            else:
-                raise ValueError('CPF inserido não é valido')
+    @staticmethod
+    def analisa_doc(doc):
+        if len(doc) == 11:
+            return Cpf(doc)
+        elif len(doc) == 14:
+            return Cnpj(doc)
         else:
-            raise ValueError('Número de digitos incorreto')
+            raise ValueError("Número de digitos incorreto!!")
+
+class Cpf:
+
+    def __init__(self, doc):
+        if self.analisa_cpf(doc):
+            self.cpf = doc
+        else:
+            raise ValueError('CPF inserido não é valido!')
+
+    def analisa_cpf(self, doc):
+        analisa_cpf = CPF()
+        return analisa_cpf.validate(doc)
 
     def formata_cpf(self):
         cpf_mask = CPF()
         return cpf_mask.mask(self.cpf)
 
     def __str__(self):
+        print("CPF salvo!")
         return self.formata_cpf()
 
-class ValidaCnpj:
-    def __init__(self,cnpj):
-        self.analisa_cnpj(cnpj)
+class Cnpj:
 
-    def analisa_cnpj(self, cnpj):
-        if len(cnpj) == 14:
-            analisa_cnpj = CNPJ()
-            if analisa_cnpj.validate(cnpj):
-                self.cnpj = cnpj
-                print('CNPJ salvo')
-            else:
-                raise ValueError('CNPJ inserido não é valido')
+    def __init__(self, doc):
+        if self.analisa_cnpj(doc):
+            self.cnpj = doc
         else:
-            raise ValueError('Número de digitos incorreto')
+            raise ValueError('CNPJ inserido não é valido!')
+
+    def analisa_cnpj(self, doc):
+        analisa_cnpj = CNPJ()
+        return analisa_cnpj.validate(doc)
 
     def formata_cnpj(self):
         cnpj_mask = CNPJ()
         return cnpj_mask.mask(self.cnpj)
 
     def __str__(self):
+        print("CNPJ salvo!")
         return self.formata_cnpj()
 
 
-# cnpj1 = '00360305000104'
-# obj_cnpj1 = ValidaCnpj(cnpj1)
-# print(obj_cnpj1,'\n')
-#
-# cpf1 = '71306554020'
-# obj_cpf1 = ValidaCpf(cpf1)
-# print(obj_cpf1)
+cnpj1 = '00360305000104'
+obj_cnpj1 = Cnpj(cnpj1)
+print(obj_cnpj1,'\n')
+
+cpf1 = '71306554020'
+obj_cpf1 = Cpf(cpf1)
+print(obj_cpf1)
 
